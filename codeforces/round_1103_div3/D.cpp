2ebc49e0e27f6dd39a1f44ec972c074cfa54e44a
Alaayed <112523580+Alaayed@users.noreply.github.com>
2026-07-19 16:55:10 +0300

    correct solution that passes in O(n(log(n))) time

--- codeforces/round_1103_div3/D.cpp
+++ codeforces/round_1103_div3/D.cpp
@@ -1,35 +1,49 @@
 #include<iostream>
 #include <vector>
 #include <algorithm>
+#include <format>
 #define all(x) x.begin(), x.end()
-int MAX_ALLOWED = 20'000;
-int MIN_ELEMENT; 
-bool roll_down(int roof, int k, std::vector<int> &arr) {
-	auto max_below_threshold = [roof](int a, int b) {
-		bool a_valid = a < roof;
-		bool b_valid = b < roof;
-		if (a_valid && b_valid) return a < b;
-		return a_valid < b_valid;
-	};
-	if (roof <= MIN_ELEMENT) return false; // cant roll down
-	int amax = * std::max_element(all(arr), max_below_threshold);
-	int acount = std::count(all(arr), amax);
-	// Second pick wins
-	if (acount % 2 == 0) return true;
-	// First pick wins, but some value exists that we jump off of.
-	if (std::any_of(all(arr), [amax,k](int n) {return ((amax-n)<=k) and n < amax;})) {
-		return true;
-	}
-	// nothing, roll down and check
-	return roll_down(amax-k, k, arr);
-}
+#define rall(x) x.rbegin(), x.rend()
 void solve() {
 	int n, k; std::cin >> n >> k;
 	std::vector<int> arr(n);
 	for (int i = 0; i < n; i++) std::cin >> arr[i];
-	MIN_ELEMENT = * std::min_element(all(arr));
-	std::cout << (roll_down(MAX_ALLOWED, k , arr) ? "YES" : "NO") << '\n';
-
+	// Sort in descending order
+	std::sort(rall(arr));
+	// Build freq table 
+	std::vector<std::pair<int,int>> freq;
+	int cur = arr[0];
+	int count = 0;
+	for (int i : arr ){
+		if (i == cur) {
+			count++;
+		} else {
+			freq.emplace_back(cur, count);
+			cur = i;
+			count = 1;
+		}
+	}
+	// push the dangling cur and count
+	freq.emplace_back(cur, count);
+	for (int i = 0; i < (int) freq.size(); i++) {
+		auto [num,count] = freq[i];
+		if (count % 2 == 0) {
+			std::cout << "YES\n";
+			return;
+		}
+		// ensure there is another item to check
+		if (i+1 == (int) freq.size()) {
+			break;
+		}
+		auto [nextNum, _] = freq[i+1];
+		// if it's in reach, jump there and egor insta wins
+		if (nextNum + k >= num) {
+			std::cout << "YES\n";
+			return;
+		}
+		// otherwise, keep searching.
+	}
+	std::cout << "NO\n";
 }
 int main() {
 	int t; std::cin >> t;
